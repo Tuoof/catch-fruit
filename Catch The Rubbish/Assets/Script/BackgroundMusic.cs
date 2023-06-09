7d4ed6d363f53ab34a26048fbac4aa194abcebdd
JanuarAfandi <januarafandi01@gmail.com>
2023-06-09 14:07:47 +0700

    Finalization
    Sudah tahap final development

---
+++ Catch The Rubbish/Assets/Script/BackgroundMusic.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+	void Start () {
+		
+	}
+
+    //Play Global
+    private static BackgroundMusic instance = null;
+    public static BackgroundMusic Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    //Play Gobal End
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
